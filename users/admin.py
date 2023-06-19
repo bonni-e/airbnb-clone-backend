874bdc88dd94bb807f9b6ebef669a25ef7528302
bonni-e <trial1bloom@gmail.com>
2023-06-19 23:26:12 +0900

    Modified users

--- users/admin.py
+++ users/admin.py
@@ -9,4 +9,30 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin) :
-    pass
+    fieldsets = (
+        ("Profile", 
+            {
+                "fields" : ("username", "password", "name", "email", "is_host")
+            },
+        ),
+        ("Permissions",
+         {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes" : ("collapse", ) # 토글로 접기 
+            },
+        ),
+        ("Important dates",
+        {  "fields" :
+             ("last_login", "date_joined"),
+            "classes" : ("collapse", ) # 토글로 접기 
+         }
+        )
+    )
+
+    list_display = ("username", "email", "name", "is_host")
--- users/models.py
+++ users/models.py
@@ -6,4 +6,12 @@ from django.contrib.auth.models import AbstractUser;
 #     pass
 
 class User(AbstractUser) :
-    pass
+    # 디폴트 필드값을 유령으로 만들기 (editable=False)
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+
+    # 나만의 이름 필드 만들기 
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
+    # is_host = models.BooleanField(null=True)    # nullable field
+    # default 속성으로 기존 값을에 대한 처리를 정해줌 
